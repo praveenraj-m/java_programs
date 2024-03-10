05eca48c605a6b9e0a7224e92bb827f26b5f078b
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-03-10 21:03:16 +0530

    03.to-do_list_app.java

--- 03.to-do_list_app.java
+++ 03.to-do_list_app.java
@@ -3,32 +3,43 @@ import java.util.Scanner;
 
 public class ToDoListApp {
     public static void main(String[] args) {
+        // Create an ArrayList to store tasks
         ArrayList<String> tasks = new ArrayList<>();
+
+        // Create a Scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
 
+        // Main program loop
         while (true) {
+            // Display menu options
             System.out.println("1. Add Task");
             System.out.println("2. View Tasks");
             System.out.println("3. Quit");
 
+            // Read user's choice
             String choice = scanner.nextLine();
 
+            // Process user's choice using a switch statement
             switch (choice) {
                 case "1":
+                    // Add a new task to the list
                     System.out.print("Enter a new task: ");
                     String task = scanner.nextLine();
                     tasks.add(task);
                     break;
                 case "2":
+                    // View all tasks in the list
                     System.out.println("Tasks:");
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println((i + 1) + ". " + tasks.get(i));
                     }
                     break;
                 case "3":
+                    // Quit the program
                     System.out.println("Goodbye!");
                     System.exit(0);
                 default:
+                    // Inform the user of an invalid choice
                     System.out.println("Invalid choice. Try again.");
             }
         }
