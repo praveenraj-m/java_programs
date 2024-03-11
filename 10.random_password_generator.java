9d943e65b1b61b706db9087f68d5e68778819b84
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-03-11 16:33:24 +0530

    10.random_password_generator.java

--- 10.random_password_generator.java
+++ 10.random_password_generator.java
@@ -2,18 +2,34 @@ import java.util.Random;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
+        // Set the desired password length
         int passwordLength = 12;
+        
+        // Generate a random password
         String generatedPassword = generateRandomPassword(passwordLength);
+        
+        // Display the generated password
         System.out.println("Generated Password: " + generatedPassword);
     }
 
+    // Function to generate a random password
     private static String generateRandomPassword(int length) {
+        // Define the characters to be used in the password
         String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?/{}[]|";
+        
+        // StringBuilder to build the password
         StringBuilder password = new StringBuilder(length);
+        
+        // Generate each character of the password randomly
         for (int i = 0; i < length; i++) {
+            // Get a random index within the range of characters
             int randomIndex = new Random().nextInt(characters.length());
+            
+            // Append the randomly selected character to the password
             password.append(characters.charAt(randomIndex));
         }
+        
+        // Convert the StringBuilder to a String and return the generated password
         return password.toString();
     }
 }
