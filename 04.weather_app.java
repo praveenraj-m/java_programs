b922968667ca6bd30dc7276a0a1b19c60d11ace7
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-03-13 15:00:13 +0530

    json Update 04.weather_app.java

--- 04.weather_app.java
+++ 04.weather_app.java
@@ -1,7 +1,8 @@
+import org.json.JSONObject;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class WeatherTest {
+public class weather {
     public static void main(String[] args) {
         // Get user input for the city name
         Scanner scanner = new Scanner(System.in);
@@ -15,11 +16,17 @@ public class WeatherTest {
         String weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
         try {
-            // Make the API request
-            String response = getWeatherData(weatherUrl);
+            // Make the API request and parse the JSON response
+            JSONObject jsonResponse = new JSONObject(getWeatherData(weatherUrl));
 
-            // Print the weather information
-            System.out.println(response);
+            // Extract the value of the "main" object
+            JSONObject mainObject = jsonResponse.getJSONObject("main");
+
+            // Extract the "temp" value from the "main" object
+            double temperature = mainObject.getDouble("temp");
+
+            // Print the temperature
+            System.out.println("Temperature: " + temperature);
         } catch (FileNotFoundException e) {
             // Handle FileNotFoundException and print a user-friendly error message
             System.out.println("Error: City not found. Please enter a valid city name.");
