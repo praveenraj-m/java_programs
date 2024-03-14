7b394554a64724dacb2c4815e69801719e576756
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-03-14 19:58:47 +0530

    06.basic_website_scraper.java

--- 06.basic_website_scraper.java
+++ 06.basic_website_scraper.java
@@ -6,17 +6,23 @@ import java.io.IOException;
 
 public class WebsiteScraper {
     public static void main(String[] args) {
+        // Define the URL of the website to be scraped
         String url = "https://www.google.com";
 
         try {
+            // Connect to the website and get the HTML document
             Document document = Jsoup.connect(url).get();
+            
+            // Select all anchor elements (links) in the HTML document
             Elements links = document.select("a");
 
+            // Iterate over each link and print its href attribute
             for (Element link : links) {
                 System.out.println(link.attr("href"));
             }
 
         } catch (IOException e) {
+            // Handle IOException by printing the stack trace
             e.printStackTrace();
         }
     }
